bd4fe1635cd0061a20014f4e91edbc7a345d378f
X99_win <X99_win.com>
2026-04-13 22:46:44 +0800

    a taste of cv2 video

--- test_cv2.py
+++ test_cv2.py
@@ -1,8 +1,31 @@
 import cv2
-import numpy
+import numpy as np
+
+
+def create_background(width: int, height: int, color: np.ndarray = np.array([255, 255, 255])) -> np.ndarray:
+    bg = np.full((height, width, 3), color, dtype=np.uint8)
+    return bg
+
+
+def create_square(frame: np.ndarray, center: tuple, width: int,
+                  color: np.ndarray = np.array([255, 255, 0])) -> np.ndarray:
+    frame[center[0]-width//2:center[0]+width//2, center[1]-width//2:center[1]+width//2] = color
+    return frame
+
+
+T, H, W = 200, 1080, 1920
 
 fps = 30
-width = 500
-height = 500
+height, width = H, W
 
-fourcc = cv2.VideoWriter_
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output_from_array.mp4', fourcc, fps, (width, height))
+
+frames = list()
+for tick in range(T):
+    frame = create_background(W, H)
+    frame = create_square(frame, center=(100, 200), width=50)
+    out.write(frame)
+
+out.release()
+print("视频已成功从 NumPy 数组写入！")
